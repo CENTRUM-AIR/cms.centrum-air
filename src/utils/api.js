8033660ca1971d2007b9a8f9b9bb052657796a65
sabrina34352 <s.babakulova@student.inha.uz>
2023-10-05 13:25:23 +0500

    authorization

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,11 +7,15 @@ const api = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_BEARER}`,
   },
 });
 
 export const formDataApi = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_BEARER}`,
+  },
 });
 export default api;
